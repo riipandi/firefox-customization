fb6405df697216ca0e9c7880084234622bb22800
Tim Ruhay <timruhay@gmail.com>
2019-04-08 14:53:15 +0700

    Initial commit

---
+++ defaults/pref/autoconfig.js
@@ -0,0 +1,10 @@
+//
+// Windows: "%APPDATA%\Mozilla\Firefox\Profiles"
+//
+
+// pref(prefName, value) – sets the user value of a preference.
+// defaultPref(prefName, value) – sets the default value of a preference.
+// lockPref(prefName, value) – sets the default value of a preference and locks it.
+
+pref("general.config.filename", "mozilla.cfg");
+pref("general.config.obscure_value", 0);
